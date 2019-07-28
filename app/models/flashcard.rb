1567f6ed5371500d5cb82c5ddf2ef510548bd927
matthewjordanau@gmail.com <matthewjordanau@gmail.com>
2019-07-27 17:41:00 -0700

    added validation for presence of fields

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,3 +1,6 @@
 class Flashcard < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
+
+  validates :language, :difficulty, :data_type, :question, :correct_answer, :incorrect_answer1, :incorrect_answer2, :incorrect_answer3, presence: true
+  
 end
