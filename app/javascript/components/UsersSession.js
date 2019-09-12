dca113c90f31069bd920977dd1baf5934043b8af
matthewjordanau@gmail.com <matthewjordanau@gmail.com>
2019-09-12 10:02:42 -0700

    added simple styling to UsersSession component for better visibility

--- app/javascript/components/UsersSession.js
+++ app/javascript/components/UsersSession.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "../../assets/stylesheets/UsersSession.scss"
 
 export default class UsersSession extends React.Component {
   render () {
@@ -6,16 +7,16 @@ export default class UsersSession extends React.Component {
     return (
       <React.Fragment>
         {!userLoggedIn &&
-          <div>
-            <h3>You are not signed in! Please Sign In or Sign Up to manage your own Flashcards!</h3>
+          <div className="messageContainer">
+            <h3 className="sessionMessage">You are not signed in! Please Sign In or Sign Up to manage your own Flashcards!</h3>
             <div>
               <a href={ userSignInRoute }>Sign In or Sign Up</a>
             </div>
           </div>
         }
         {userLoggedIn &&
-          <div>
-            <h3>You are signed in!</h3>
+          <div className="messageContainer">
+            <h3 className="sessionMessage">You are signed in!</h3>
             <div>
               <a href={ userSignOutRoute }>Sign Out</a>
             </div>
